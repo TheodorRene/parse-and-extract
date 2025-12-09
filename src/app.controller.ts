a42f4be12fda30051548da484d40a3194bda5b40
Theodor René Carlsen <theodor.rene@gmail.com>
2025-12-09 15:57:36 +0100

    add file upload to prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
@@ -10,8 +19,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/html')
+  @Get('html')
   getHtml(): string {
     return this.appService.getHtml();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    // Handle the uploaded file here
+    this.logger.log(`Env: ${process.env.DATABASE_URL}`);
+    this.logger.log(file, 'Uploaded File');
+    await this.appService.handleUploadedFile(file);
+    return `File ${file.originalname} uploaded successfully.`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import fs from 'fs';
 import { PDFParse } from 'pdf-parse';
 import { JSDOM } from 'jsdom';
 import { Readability } from '@mozilla/readability';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): Promise<string> {
     return readPdf('testfiles/curia.pdf');
   }
   getHtml(): string {
     return readHtml('testfiles/mfkn.html');
   }
+  async getText(): Promise<string> {
+    const document = await this.prisma.document.findUnique({
+      where: { id: 1 },
+    });
+    return document?.content || 'No document found';
+  }
+  async handleUploadedFile(file: Express.Multer.File): Promise<void> {
+    this.logger.log(
+      `Handling uploaded file: ${file.originalname} with mimetype: ${file.mimetype}`,
+    );
+    const fileType =
+      file.mimetype === 'application/pdf'
+        ? 'pdf'
+        : file.mimetype === 'text/html'
+          ? 'html'
+          : 'unknown';
+
+    let content = '';
+    if (fileType === 'pdf') {
+      content = await readPdfFromBuffer(file.buffer);
+    } else if (fileType === 'html') {
+      content = readHtmlFromBuffer(file.buffer);
+    } else {
+      throw new Error(`Unsupported file type: ${file.mimetype}`);
+    }
+
+    const createDocument = await this.prisma.document.create({
+      data: {
+        title: file.originalname,
+        content,
+        type: fileType,
+      },
+    });
+    this.logger.log('Created document:', createDocument);
+  }
+}
+async function readPdfFromBuffer(buffer: Buffer): Promise<string> {
+  const data = new PDFParse({ data: buffer });
+  const text = await data.getText();
+  return text.text;
 }
 
 async function readPdf(path: string): Promise<string> {
@@ -19,6 +63,16 @@ async function readPdf(path: string): Promise<string> {
   const data = new PDFParse({ data: buffer });
   const text = await data.getText();
   return text.text;
+}
+
+function readHtmlFromBuffer(buffer: Buffer): string {
+  const dom = new JSDOM(buffer.toString('utf-8'));
+  const reader = new Readability(dom.window.document);
+  const article = reader.parse();
+  if (!article || !article.content) {
+    throw new Error('Could not parse HTML document');
+  } // Maybe convert to markdown?
+  return article.content;
 }
 
 function readHtml(path: string): string {
